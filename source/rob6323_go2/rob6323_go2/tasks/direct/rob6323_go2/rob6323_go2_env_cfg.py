569390a7836f185e38db461273ac75943211df24
Alex Gonzalez <adgonzalez7@yahoo.com>
2025-12-01 13:25:15 -0500

    Add files via upload
    fixed tab issues

--- source/rob6323_go2/rob6323_go2/tasks/direct/rob6323_go2/rob6323_go2_env_cfg.py
+++ source/rob6323_go2/rob6323_go2/tasks/direct/rob6323_go2/rob6323_go2_env_cfg.py
@@ -1,6 +1,5 @@
-# Copyright (c) 2022-2025, The Isaac Lab Project Developers (https://github.com/isaac-sim/IsaacLab/blob/main/CONTRIBUTORS.md).
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers
 # All rights reserved.
-#
 # SPDX-License-Identifier: BSD-3-Clause
 
 from isaaclab_assets.robots.unitree import UNITREE_GO2_CFG
@@ -15,6 +14,7 @@ from isaaclab.utils import configclass
 from isaaclab.terrains import TerrainImporterCfg
 from isaaclab.sensors import ContactSensorCfg
 from isaaclab.markers import VisualizationMarkersCfg
+from isaaclab.actuators import ImplicitActuatorCfg  # Fixed import
 from isaaclab.markers.config import BLUE_ARROW_X_MARKER_CFG, FRAME_MARKER_CFG, GREEN_ARROW_X_MARKER_CFG
 
 @configclass
@@ -25,7 +25,7 @@ class Rob6323Go2EnvCfg(DirectRLEnvCfg):
     # - spaces definition
     action_scale = 0.25
     action_space = 12
-    observation_space = 48
+    observation_space = 48 + 4  # Added 4 for clock inputs (moved from later)
     state_space = 0
     debug_vis = True
     
@@ -33,12 +33,11 @@ class Rob6323Go2EnvCfg(DirectRLEnvCfg):
     # reward scales: 
     orient_reward_scale = -5.0
     lin_vel_z_reward_scale = -0.02
-    ang_vel_xy_reward_scale = -.001
+    ang_vel_xy_reward_scale = -0.001
     
     # ACTION REGULARIZATION AND SMOOTHNESS
-    dof_vel_reward_scale = -.0001	# small val, penalize high joint velocities
-    torque_reward_scale = -.00001	# smalelr val, penalize high torques
-    
+    dof_vel_reward_scale = -0.0001   # small val, penalize high joint velocities
+    torque_reward_scale = -0.00001   # smaller val, penalize high torques
     
     # part 1
     # reward scales
@@ -46,31 +45,18 @@ class Rob6323Go2EnvCfg(DirectRLEnvCfg):
     
     # part 2
     # PD control gains
-    Kp = 20.0				# proportional gain
-    Kd = .5					# derivative gain
-    torque_limits = 100.0	# Max torque
+    Kp = 20.0                # proportional gain
+    Kd = 0.5                 # derivative gain
+    torque_limits = 100.0    # Max torque
 
-	# update robot_cfg
-	robot_cfg: ArticulationCfg = UNITREE_GO2_CFG.replace(prim_path="/World/envs/env_.*/Robot")
-	# "base_legs" is an arbitrary key we use to group these actuators
-	robot_cfg.actuators["base_legs"] = ImplicitActuatorCfg(
-		joint_names_expr=[".*_hip_joint", ".*_thigh_joint", ".*_calf_joint"],
-		effort_limit=23.5,
-		velocity_limit=30.0,
-		stiffness=0.0,		# critical: set to 0 to disable implicit p-gain
-		damping=0.0,		# Critical: set to 0 to disable implicit d-gain
-	)
-
-	# part 3 
-	base_height_min = 0.2 	# terminate if base is lower than 20cm
-	
-	# part 4
-	observation_space = 48 + 4	# added 4 for clock inputs
-	
-	raibert_heuristic_reward_scale = -10.0
-	feet_clearance_reward_scale = -30.0
-	tracking_contacts_shaped_force_reward_scale = 4.0
-	
+    # part 3 
+    base_height_min = 0.2    # terminate if base is lower than 20cm
+    
+    # part 4
+    raibert_heuristic_reward_scale = -10.0
+    feet_clearance_reward_scale = -30.0
+    tracking_contacts_shaped_force_reward_scale = 4.0
+    
     # simulation
     sim: SimulationCfg = SimulationCfg(
         dt=1 / 200,
@@ -83,6 +69,7 @@ class Rob6323Go2EnvCfg(DirectRLEnvCfg):
             restitution=0.0,
         ),
     )
+    
     terrain = TerrainImporterCfg(
         prim_path="/World/ground",
         terrain_type="plane",
@@ -96,14 +83,30 @@ class Rob6323Go2EnvCfg(DirectRLEnvCfg):
         ),
         debug_vis=False,
     )
+    
     # robot(s)
     robot_cfg: ArticulationCfg = UNITREE_GO2_CFG.replace(prim_path="/World/envs/env_.*/Robot")
+    
+    # update robot_cfg - Part 2: Custom PD Controller
+    # "base_legs" is an arbitrary key we use to group these actuators
+    robot_cfg.actuators["base_legs"] = ImplicitActuatorCfg(
+        joint_names_expr=[".*_hip_joint", ".*_thigh_joint", ".*_calf_joint"],
+        effort_limit=23.5,
+        velocity_limit=30.0,
+        stiffness=0.0,   # critical: set to 0 to disable implicit p-gain
+        damping=0.0,     # Critical: set to 0 to disable implicit d-gain
+    )
 
     # scene
     scene: InteractiveSceneCfg = InteractiveSceneCfg(num_envs=4096, env_spacing=4.0, replicate_physics=True)
+    
     contact_sensor: ContactSensorCfg = ContactSensorCfg(
-        prim_path="/World/envs/env_.*/Robot/.*", history_length=3, update_period=0.005, track_air_time=True
+        prim_path="/World/envs/env_.*/Robot/.*", 
+        history_length=3, 
+        update_period=0.005, 
+        track_air_time=True
     )
+    
     goal_vel_visualizer_cfg: VisualizationMarkersCfg = GREEN_ARROW_X_MARKER_CFG.replace(
         prim_path="/Visuals/Command/velocity_goal"
     )
